7233a27e881fb1a2992fe888318be3ad5c9cb446
h-alekyan <hovhannes.alekyan@minerva.kgi.edu>
2022-01-07 13:06:35 +0100

    initial commit

---
+++ tests/EthSwap.test.js
@@ -0,0 +1,16 @@
+const { Item } = require('react-bootstrap/lib/Breadcrumb')
+
+const Token = artifacts.require('Token')
+const EthSwap = artifacts.require('EthSwap')
+
+require('chai').use(require('chai-as-promised')).should()
+
+contract('EthSwap', (accounts) => {
+    describe("EthSwap deployment", async() => {
+        Item("contract has a name", async() => {
+            let ethSwap = await EthSwap.new()
+            const name = await ethSwap.name()
+            assert.equal(name, "EthSwap Instant Exchange")
+        })
+    })
+})
