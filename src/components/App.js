30274a47882a9dc8b9cc4f6f158d4db5463bd354
h-alekyan <hovhannes.alekyan@minerva.kgi.edu>
2022-01-07 22:00:15 +0100

    add buy/sell smart contracts

--- src/components/App.js
+++ src/components/App.js
@@ -20,25 +20,9 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                
+                <h1>Hello</h1>
+        
               </div>
             </main>
           </div>
