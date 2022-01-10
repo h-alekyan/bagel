daf4093f1c48c7c691f3609352f9b2362330938b
h-alekyan <hovhannes.alekyan@minerva.kgi.edu>
2022-01-10 23:43:43 +0100

    more frontend & rebrand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react'
 import Identicon from 'identicon.js';
+import tokenLogo from '../token-logo.png'
 
 class Navbar extends Component {
   
 
   render() {
     return (
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          EthSwap
-        </a>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-10 shadow">
+        <div className="text-white font-weight-bold">
+        <img src={tokenLogo} height='32' alt=""/>
+        &nbsp;  Bagel
+          
+        </div>
         <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
                 <small className="text-secondary">
